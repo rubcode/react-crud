a0880a72708a498c066b91b45281aec45d62afda
rubcode <rorr930223@gmail.com>
2025-02-24 09:25:35 -0600

    Login

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,18 +1,27 @@
 import Layout from "./Containers/layout"
 import FormLogin from "./Forms/form-login"
+import Nav from "./Containers/nav"
 import { useState } from "react"
 
 function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     return (
-        <Layout>
-            <FormLogin
-                setEmail={setEmail}
-                setPassword={setPassword}
-                
-            />
-        </Layout>
+        <>
+            <Nav>
+                <ul className='items'>
+                        <li key="nav_menu_tareas"><a href='Tasks'>Tareas</a></li>
+                </ul>
+            </Nav>
+            <Layout>
+                <FormLogin
+                    setEmail={setEmail}
+                    setPassword={setPassword}
+                    
+                />
+            </Layout>
+            </>
+        
     )
 }
 
